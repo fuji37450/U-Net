754e4863a907d0c1d54dc84667408c7c3e99525f
fuji37450 <40747030s@ntnu.edu.tw>
2022-10-26 15:31:37 +0800

    feat: update unet module

--- train.py
+++ train.py
@@ -1,12 +1,11 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
 import os
-# from tensorboardX import SummaryWriter
 import time
 from tqdm import tqdm
+from collections import defaultdict
 
 from dataloader import SignatureLoader
 from model.unet import UNet
@@ -40,60 +39,73 @@ def train():
     test_loader = torch.utils.data.DataLoader(
         test_set, batch_size=2*BATCH_SIZE, shuffle=False)
 
-    model = UNet(n_channels=3, n_classes=1).to(device)
+    model = UNet(n_class=1).to(device)
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.RMSprop(
-        model.parameters(), lr=LEARNING_RATE, weight_decay=1e-8, momentum=0.9)
-    amp = False
-    grad_scaler = torch.cuda.amp.GradScaler(enabled=amp)
-
-    # writer = SummaryWriter(log_dir='scalar')
+    optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=1e-4)
 
     criterion = criterion.to(device)
-    iter_n = 0
     t = time.strftime("%m-%d-%H-%M", time.localtime())
 
-    best_test_score = 0
+    best_test_loss = 1e10
+    best_train_loss = 1e10
     for epoch in tqdm(range(1, EPOCHS + 1)):
-        for i, (imgs, true_masks) in enumerate(tqdm(train_loader)):
-            torch.cuda.empty_cache()
+        epoch_samples = 0
+        
+        for imgs, true_masks in tqdm(train_loader):
+            model.train()
+            metrics = defaultdict(float)
 
+            imgs = imgs.to(device)
+            true_masks = true_masks.to(device)
+
+            # zero the parameter gradients
             optimizer.zero_grad()
 
-            imgs, true_masks = imgs.to(device), true_masks.to(device)
-            masks_pred = model(imgs)
+            # forward
+            # track history if only in train
+            outputs = model(imgs)
+            loss = calc_loss(outputs, true_masks, metrics)
 
-            loss = criterion(masks_pred.reshape(32, 128, 128), true_masks) \
-                + dice_loss(F.softmax(masks_pred, dim=1).float(),
-                            F.one_hot(true_masks, model.n_classes).permute(
-                                0, 3, 1, 2).float(),
-                            multiclass=True)
+            # backward + optimize only if in training phase
+            loss.backward()
+            optimizer.step()
 
-            grad_scaler.scale(loss).backward()
-            grad_scaler.step(optimizer)
-            grad_scaler.update()
+            # statistics
+            epoch_samples += imgs.size(0)
 
-            # writer.add_scalar(
-            #     f'{args.model_prefix} {t}/train_loss', loss.item(), iter_n)
-            # writer.add_scalar(
-            #     f'{args.model_prefix} {t}/train_accuracy', accuracy, iter_n)
-            # print(f'loss: {loss.item()}, accuracy: {accuracy}')
+        train_loss = metrics['dice'] / epoch_samples
+        if train_loss < best_train_loss:
+            print("saving best train model")
+            best_train_loss = train_loss
+            torch.save(model.state_dict(), f'pths/train_{metrics["dice"]:.2f}.pth')
+        print_metrics(metrics, epoch_samples)
+        
+        epoch_samples = 0
+        for imgs, true_masks in tqdm(test_loader):
+            model.eval()
+            metrics = defaultdict(float)
 
-            # if (i + 1) % 100 == 0:
-            test_score = evaluate(model, test_loader, device)
-            print(f'test score:{test_score:.6f}')
-            if test_score >= best_test_score:
-                best_test_score = test_score
-                torch.save(model.state_dict(),
-                           f'{args.model_prefix}_{test_score:%}.pth')
+            imgs = imgs.to(device)
+            true_masks = true_masks.to(device)
 
-            iter_n += 1
+            # zero the parameter gradients
+            optimizer.zero_grad()
 
-            # print('Epoch[{}/{}], iter {}, loss:{:.6f}, accuracy:{}'.format(epoch,
-            #                                                                EPOCHS, i, loss.item(), accuracy))
+            # forward
+            outputs = model(imgs)   
+            loss = calc_loss(outputs, true_masks, metrics)
 
-    # writer.close()
+            # statistics
+            epoch_samples += imgs.size(0)
+
+        test_loss = metrics['dice'] / epoch_samples
+        if test_loss < best_test_loss:
+            print("saving best model")
+            best_test_loss = test_loss
+            torch.save(model.state_dict(), f'pths/test_{metrics["dice"]:.2f}.pth')
+        print_metrics(metrics, epoch_samples, phase='test')
+        
 
 
 if __name__ == '__main__':
